795a12fa782e19ccaefd19cb30d8a23d4d0269b9
JacketHeee <3123560050@sv.sgu.edu.vn>
2025-03-27 09:58:05 +0700

    test

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -1,5 +1,16 @@
 package GUI;
 
-public class Login {
+import javax.swing.JFrame;
+
+public class Login extends JFrame{
+
+    public Login() {
+        init(); 
+    }
     
+    private void init() {
+        setSize(500,400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLaf;
+
+import GUI.Login;
+
 public class Main {
+
+	public static void main(String[] args) {
+		FlatIntelliJLaf.setup(); 
+		FlatLaf.registerCustomDefaultsSource("");
+		java.awt.EventQueue.invokeLater(() -> new Login().setVisible(true));
+	}
     
 }
